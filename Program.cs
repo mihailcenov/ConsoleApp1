f8fb30ba2b2bf443f270df3cc593d3cae7672cbc
mihailcenov <mihailceov@gmail.com>
2023-12-03 22:09:05 +0200

    Added example for switch operator.

--- Program.cs
+++ Program.cs
@@ -41,3 +41,42 @@ do
     Console.WriteLine("{0}", k);
     k--;
 } while (k >= -10);
+
+int x = 16;
+
+switch (x) {
+    case 1:
+        Console.WriteLine("the selection is 1");
+        break;
+    case 2:
+        Console.WriteLine("the selection is 2");
+        break;
+    case 3:
+        Console.WriteLine("the selection is 3");
+        break;
+    case 4:
+        Console.WriteLine("the selection is 4");
+        break;
+    case 5:
+        Console.WriteLine("the selection is 5");
+        break;
+    case 6:
+        Console.WriteLine("the selection is 6");
+        break;
+    case 7:
+        Console.WriteLine("the selection is 7");
+        break;
+    case 8:
+        Console.WriteLine("the selection is 8");
+        break;
+    case 9:
+        Console.WriteLine("the selection is 9");
+        break;
+    case 10:
+        Console.WriteLine("the selection is 10");
+        break;
+    default:
+        Console.WriteLine("Something else found: {0}",x);
+        break;
+}
+
